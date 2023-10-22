9738c88c873104430764f811dabaaae733e86c5d
ebevec <eb1349@georgetown.edu>
2023-10-21 20:11:01 -0400

    Homework 3 Commits

--- dsan-website/5000-website/pages/datacleaning/datacleaning_weo.R
+++ dsan-website/5000-website/pages/datacleaning/datacleaning_weo.R
@@ -1,7 +1,7 @@
 
 library(tidyverse)
 
-weo <- read.csv('../../../../data/00-raw-data/WEOApr2023all.csv', na.strings = c('n/a'))
+weo <- read.csv('C:\\Users\\erinb\\OneDrive\\Documents\\Georgetown\\DSAN 5000\\dsan-5000-project-ebevec\\data\\00-raw-data\\WEOApr2023all.csv', na.strings = c('n/a'))
 head(weo)
 
 colMeans(is.na(weo))
@@ -19,3 +19,5 @@ str(weo)
 weo <- weo %>% mutate_at(vars(X1990, X1990, X1992, X1993, X1994, X1995, X1996, X1997, X1998, X1999, X2000, X2001, X2002, X2003, X2004, X2005, X2006, X2007, X2008, X2009, X2010, X2011, X2012, X2013, X2014, X2015, X2016, X2017, X2018, X2019, X2021, X2022, X2023), as.numeric)
 
 str(weo)
+
+write.csv(weo, 'C:\\Users\\erinb\\OneDrive\\Documents\\Georgetown\\DSAN 5000\\dsan-5000-project-ebevec\\dsan-website\\5000-website\\pages\\datacleaning\\data_weo_clean.csv')
